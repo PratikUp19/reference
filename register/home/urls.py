a7c03cf7197b2b516ea614371a82f7e5cea2ae88
PratikUp19 <84855152+PratikUp19@users.noreply.github.com>
2022-12-05 23:22:21 +0530

    Add files via upload

---
+++ register/home/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import include, path
+from home import views
+
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('register', views.register, name="handleregister"),
+    path('log', views.log, name="handlelog"),
+    path('signup', views.handleSign, name="handleSignUp"),
+    path('login', views.handeLogin, name="handleLogin"),
+    path('logout', views.handelLogout, name="handleLogout"),
+    # path('/', include('blog.urls'))
+]
